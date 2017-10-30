d948c44776c008ed20ca63f9f0b076fa6f3b4cf0
amisbah <31697437+amisbah@users.noreply.github.com>
2017-10-29 23:03:32 -0400

    Update flickr.py

--- flickr.py
+++ flickr.py
@@ -56,9 +56,10 @@ class Photo:
         self.title = photo_dict['title']
         self.id = photo_dict['id']
         self.owner = photo_dict['owner']
+        self.owner_username = photo_dict['owner']['username']
 
     def __str__(self):
-        return '{0} by {1}'.format(self.title, self.owner)
+        return '{0} by {1}'.format(self.title, self.owner_username)
 
 
 CACHE_DICTION = load_cache_json()
